2a4c65d00d332c56c3714dfde6039683553e513f
DAWIT TESFAYE <123860794+Dawi6825github@users.noreply.github.com>
2025-04-14 06:06:32 +0300

    Add bingo-frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js 1744599990627

--- bingo-frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ bingo-frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "105f93a36460903e"; }
+/******/ 	__webpack_require__.h = function() { return "f2552bb1fce159ff"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
