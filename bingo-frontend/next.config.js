fefc1e039d9412ce6add1a22bf02a1112704cacf
DAWIT TESFAYE <123860794+Dawi6825github@users.noreply.github.com>
2025-04-14 06:02:53 +0300

    Add bingo-frontend/next.config.js 1744599772206

--- bingo-frontend/next.config.js
+++ bingo-frontend/next.config.js
@@ -12,6 +12,19 @@ const nextConfig = {
             }
         ];
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: 'http://localhost:8000' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+                ]
+            }
+        ];
+    },
     webpack: (config) => {
         config.externals = [...config.externals, 'canvas', 'jsdom'];
         return config;
